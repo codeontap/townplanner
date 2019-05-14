5c4238a55349f6e3cad4b4af2333a84c177f313d
Ilya Nemykin <ilhanhtml@gmail.com>
2019-05-14 14:41:54 +0300

    Added document collections to main navigation side bar #1

--- dev/pages/business/walks/docs/index.md
+++ dev/pages/business/walks/docs/index.md
@@ -1,3 +1,6 @@
+---
+---
+
 # Walks Domain
 
 The walks domain is responsible for the registration of walks within the ministry of silly walks
--- src/js/app.js
+++ src/js/app.js
@@ -3,15 +3,7 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
-        console.log(this);
-        console.log(this.parentElement);
         this.parentElement.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight){
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
     });
 }
 
